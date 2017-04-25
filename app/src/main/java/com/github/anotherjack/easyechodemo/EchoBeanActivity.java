288092e763827952bf7f4d9735688c00e83c5386
zhengj <x614545287@gmail.com>
2017-04-25 12:43:24 +0800

    修改

--- app/src/main/java/com/github/anotherjack/easyechodemo/EchoBeanActivity.java
+++ app/src/main/java/com/github/anotherjack/easyechodemo/EchoBeanActivity.java
@@ -2,13 +2,11 @@ package com.github.anotherjack.easyechodemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import com.github.anotherjack.easyechodemo.bean.Student;
 import com.github.anotherjack.easyechodemo.easyEcho.EasyEcho;
 import com.google.gson.Gson;
 
-import java.lang.reflect.Field;
 
 public class EchoBeanActivity extends AppCompatActivity {
     private String data_str;
@@ -33,38 +31,4 @@ public class EchoBeanActivity extends AppCompatActivity {
         });
     }
 
-
-    private void echoBean(Object obj){
-        Class clazz = obj.getClass();
-        Field[] fields = clazz.getDeclaredFields();
-        for(Field field : fields){
-            field.setAccessible(true);
-            //获取属性
-            String key = field.getName();
-            //获取值
-            String value = null;
-            try {
-                value = field.get(obj).toString();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-
-
-            int id = 0;
-            //通过反射获取到控件id
-            try {
-                Field idField = R.id.class.getDeclaredField(key);
-                id = idField.getInt(R.id.class);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            //最后控件赋值，大功告成
-            TextView tv = (TextView) findViewById(id);
-            if(tv!=null){
-                tv.setText(value);
-            }
-
-        }
-    }
 }
--- app/src/main/java/com/github/anotherjack/easyechodemo/EchoMapActivity.java
+++ app/src/main/java/com/github/anotherjack/easyechodemo/EchoMapActivity.java
@@ -2,15 +2,12 @@ package com.github.anotherjack.easyechodemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 
 import com.github.anotherjack.easyechodemo.easyEcho.EasyEcho;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 public class EchoMapActivity extends AppCompatActivity {
@@ -53,33 +50,4 @@ public class EchoMapActivity extends AppCompatActivity {
         EasyEcho.echoMap(studentMap, this);
     }
 
-
-
-
-    private void echoMap(Map map){
-        Iterator entries = map.entrySet().iterator();
-        while (entries.hasNext()){
-            Map.Entry entry = (Map.Entry) entries.next();
-            //获取到键
-            String key = entry.getKey().toString();
-            //获取到值
-            String value = entry.getValue().toString();
-
-
-            int id = 0;
-            //通过反射获取到控件id
-            try {
-                Field idField = R.id.class.getDeclaredField(key);
-                id = idField.getInt(R.id.class);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            //最后控件赋值，大功告成
-            TextView tv = (TextView) findViewById(id);
-            if(tv!=null){
-                tv.setText(value);
-            }
-        }
-    }
 }
